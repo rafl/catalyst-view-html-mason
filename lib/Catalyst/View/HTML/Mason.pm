ef722856afe7ae124575476ae84befd23273570e
Florian Ragwitz <rafl@debian.org>
2009-11-14 16:43:31 +0100

    Add some docs.

--- lib/Catalyst/View/HTML/Mason.pm
+++ lib/Catalyst/View/HTML/Mason.pm
@@ -2,8 +2,7 @@ package Catalyst::View::HTML::Mason;
 
 use Moose;
 use Try::Tiny;
-use MooseX::Types::Moose
-    qw/ArrayRef HashRef ClassName Str Bool Object CodeRef/;
+use MooseX::Types::Moose qw/ArrayRef HashRef ClassName Str Bool Object CodeRef/;
 use MooseX::Types::Structured qw/Tuple/;
 use Encode::Encoding;
 use Data::Visitor::Callback;
@@ -12,12 +11,51 @@ use namespace::autoclean;
 
 extends 'Catalyst::View';
 
+=head1 SYNOPSIS
+
+    package MyApp::View::Mason;
+
+    use Moose;
+    use namespace::autoclean;
+
+    extends 'Catalyst::View::HTML::Mason';
+
+    __PACKAGE__->config(
+        interp_args => {
+            comp_root => MyApp->path_to('root'),
+        },
+    );
+
+    1;
+
+=head1 DESCRIPTION
+
+This module provides rendering of HTML::Mason templates for Catalyst
+applications.
+
+It's basically a rewrite of L<Catalyst::View::Mason|Catalyst::View::Mason>,
+which became increasingly hard to maintain over time, while keeping backward
+compatibility.
+
+=attr interp
+
+The mason interpreter instance responsible for rendering templates.
+
+=cut
+
 has interp => (
     is      => 'ro',
     isa     => Object,
     lazy    => 1,
     builder => '_build_interp',
 );
+
+=attr interp_class
+
+The class the C<interp> instance is constructed from. Defaults to
+C<HTML::Mason::Interp>.
+
+=cut
 
 {
     use Moose::Util::TypeConstraints;
@@ -33,11 +71,26 @@ has interp => (
     );
 }
 
+=attr interp_args
+
+Arguments to be passed to the construction of C<interp>. Defaults to an empty
+hash reference.
+
+=cut
+
 has interp_args => (
     is      => 'ro',
     isa     => HashRef,
-    default => sub{ +{} },
+    default => sub { +{} },
 );
+
+=attr template_extension
+
+File extension to be appended to every component file. By default it's only
+appended if no explicit component file has been provided in
+C<< $ctx->stash->{template} >>.
+
+=cut
 
 has template_extension => (
     is      => 'ro',
@@ -45,45 +98,69 @@ has template_extension => (
     default => '',
 );
 
+=attr always_append_template_extension
+
+If this is set to a true value, C<template_extension> will also be appended to
+component paths provided in C<< $ctx->stash->{template} >>.
+
+=cut
+
 has always_append_template_extension => (
     is      => 'ro',
     isa     => Bool,
     default => 0,
 );
 
+=attr encoding
+
+FIXME
+
+=cut
+
 {
+    my $tc = subtype as 'Encode::Encoding';
+    coerce $tc, from Str, via { Encode::find_encoding($_) };
 
-  my $tc = subtype as 'Encode::Encoding';
-  coerce $tc, from Str, via { Encode::find_encoding( $_ ) };
-
-  has encoding => (
-    is     => 'ro',
-    isa    => $tc,
-    coerce => 1,
-  );
-
+    has encoding => (
+        is        => 'ro',
+        isa       => $tc,
+        coerce    => 1,
+        predicate => 'has_encoding',
+    );
 }
+
+=attr globals
+
+FIXME
+
+=cut
 
 {
     my $glob_spec = subtype as Tuple[Str,CodeRef];
     coerce $glob_spec, from Str, via {
-        my ( $type, $var ) = split( qr//, $_, 2 );
-        my $fn   = {
-           '$' => sub{ $_[0] },
-           '@' => sub{
-               return unless defined $_[0];
-               ref $_[0] eq 'ARRAY'? @{$_[0]}: !ref $_[0]? $_[0]: ();
-           },
-           '%' => sub{
-               return unless defined $_[0];
-               ref $_[0] eq 'HASH' ? %{$_[0]} : ();
-           },
+        my ($type, $var) = split q//, $_, 2;
+        my $fn = {
+            '$' => sub { $_[0] },
+            '@' => sub {
+                return unless defined $_[0];
+                ref $_[0] eq 'ARRAY'
+                    ? @{ $_[0] }
+                    : !ref $_[0]
+                        ? $_[0]
+                        : ();
+            },
+            '%' => sub {
+                return unless defined $_[0];
+                ref $_[0] eq 'HASH'
+                    ? %{ $_[0] }
+                    : ();
+            },
         }->{ $type };
-        [ $_ => sub{ $fn->( $_[1]->stash->{ $var })} ];
+        [$_ => sub { $fn->( $_[1]->stash->{$var} ) }];
     };
 
-    my $tc = subtype as ArrayRef[ $glob_spec ];
-    coerce $tc, from ArrayRef, via{ [ map{ $glob_spec->coerce( $_ ) } @$_ ]};
+    my $tc = subtype as ArrayRef[$glob_spec];
+    coerce $tc, from ArrayRef, via { [map { $glob_spec->coerce($_) } @{ $_ } ]};
 
     has globals => (
         is      => 'ro',
@@ -106,19 +183,19 @@ sub _build_interp {
     my ($self) = @_;
 
     my %args = %{ $self->interp_args };
-    if ( my $enc = $self->encoding ) {
-        my $old_func = delete $args{ postprocess_text };
-        $args{ postprocess_text } = sub{
-            $old_func->( $_[0] ) if $old_func;
-            ${$_[0]} = $enc->decode( ${$_[0]} );
+    if ($self->has_encoding) {
+        my $old_func = delete $args{postprocess_text};
+        $args{postprocess_text} = sub {
+            $old_func->($_[0]) if $old_func;
+            ${ $_[0] } = $self->encoding->decode(${ $_[0] });
         };
     }
 
     $args{allow_globals} ||= [];
-    unshift @{ $args{allow_globals}}, map{ $_->[0] } @{ $self->globals };
+    unshift @{ $args{allow_globals}}, map { $_->[0] } @{ $self->globals };
 
-    $args{in_package} ||= sprintf '%s::Commands', do{
-        if ( my $meta = Class::MOP::class_of( $self )) {
+    $args{in_package} ||= sprintf '%s::Commands', do {
+        if (my $meta = Class::MOP::class_of($self)) {
             $meta->name;
         } else {
             ref $self;
@@ -126,23 +203,23 @@ sub _build_interp {
     } ;
 
     my $v = Data::Visitor::Callback->new(
-        'Path::Class::Entity' => sub{ blessed $_ ? $_->stringify : $_ },
+        'Path::Class::Entity' => sub { blessed $_ ? $_->stringify : $_ },
     );
 
-    return $self->interp_class->new( $v->visit( %args ) );
+    return $self->interp_class->new( $v->visit(%args) );
 }
 
 sub render {
     my ($self, $ctx, $comp, $args) = @_;
     my $output = '';
 
-    for ( @{ $self->globals } ) {
-        my ( $decl, @values ) = ( $_->[0] => $_->[1]->($self, $ctx));
-        if ( @values ) {
-            $self->interp->set_global( $decl, @values );
+    for (@{ $self->globals }) {
+        my ($decl, @values) = ($_->[0] => $_->[1]->($self, $ctx));
+        if (@values) {
+            $self->interp->set_global($decl, @values);
         } else {
             # HTML::Mason::Interp->set_global would crash on empty lists
-            $self->_unset_interp_global( $decl );
+            $self->_unset_interp_global($decl);
         }
     }
 
@@ -213,10 +290,10 @@ sub _get_component {
 }
 
 sub _unset_interp_global {
-    my ( $self, $decl ) = @_;
-    my ( $prefix, $name ) = split( qr//, $decl, 2 );
+    my ($self, $decl) = @_;
+    my ($prefix, $name) = split q//, $decl, 2;
     my $package = $self->interp->compiler->in_package;
-    my $varname = sprintf( "%s::%s", $package, $name );
+    my $varname = sprintf "%s::%s", $package, $name;
 
     no strict 'refs';
     if    ($prefix eq '$') { $$varname = undef }
